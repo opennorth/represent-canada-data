a8322d390ac07c3cb2799041e7d70519c657f1fe
James McKinney <james@slashpoundbang.com>
2012-05-15 13:01:42 -0400

    add halifax

---
+++ ns/halifax/ed/definition.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register('Halifax wards',
+    domain='Halifax, NS',
+    last_updated=date(2012, 5, 15),
+    name_func=boundaries.attr('DISTNAME'),
+    id_func=boundaries.attr('DIST_ID'),
+    authority='Halifax Regional Municipality',
+    notes='We use a shapefile received via email.',
+    encoding='iso-8859-1',
+)
