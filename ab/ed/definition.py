1a722d7dd48bc7d7a8c5d36163e0b29310009487
James McKinney <james@slashpoundbang.com>
2012-05-23 19:20:58 -0400

    switch to latest AB ridings

--- ab/ed/definition.py
+++ ab/ed/definition.py
@@ -3,6 +3,7 @@ from datetime import date
 import boundaries
 
 boundaries.register('Alberta electoral districts',
+    singular='Alberta electoral district',
     domain='Alberta',
     last_updated=date(2012, 12, 9),
     name_func=boundaries.clean_attr('EDNAME'),
@@ -10,7 +11,6 @@ boundaries.register('Alberta electoral districts',
     authority='Government of Alberta',
     source_url='http://www.altalis.com/prod_base_bound.html',
     licence_url='http://www.altalis.com/agreement.html',
-    data_url='http://www.altalis.com/Samples/Provincial%20Electoral%20Divisions%202003.zip',
-    notes='The Wetaskiwin-Camrose district is split into two features. We merge them using Quantum GIS.',
+    data_url='http://www.altalis.com/Samples/Provincial%20Electoral%20Divisions.zip',
     encoding='iso-8859-1',
 )
