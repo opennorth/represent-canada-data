ed66034781db9bb4a8e9ecebd7d4bbe3159faff0
James McKinney <james@slashpoundbang.com>
2012-07-21 14:06:45 -0400

    add grande prairie county no. 1

---
+++ ab/grandeprairieno1/definition.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register('Grande Prairie County No. 1 divisions',
+    domain='Grande Prairie County No. 1, AB',
+    last_updated=date(2012, 6, 18),
+    name_func=lambda f: 'Division %s' % f.get('ElectoralID'),
+    id_func=boundaries.attr('ElectoralID'),
+    authority='County of Grande Prairie No. 1',
+    source_url='http://data.countygp.ab.ca/',
+    licence_url='https://www.countygp.ab.ca/municipal/countygp/countygp-website.nsf/AllDoc/462C5D3B8363A1DE872579F00074012A?OpenDocument',
+    data_url='http://data.countygp.ab.ca/data/CouncillorDivisions/Councillor_Divisions.zip',
+    encoding='iso-8859-1',
+)
