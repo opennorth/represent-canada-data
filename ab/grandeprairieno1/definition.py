8380e08ee0f8976c0e476eeb2c6fe37d5e3daa0a
James McKinney <james@slashpoundbang.com>
2012-07-21 14:09:37 -0400

    fix typo

--- ab/grandeprairieno1/definition.py
+++ ab/grandeprairieno1/definition.py
@@ -5,7 +5,7 @@ import boundaries
 boundaries.register('Grande Prairie County No. 1 divisions',
     domain='Grande Prairie County No. 1, AB',
     last_updated=date(2012, 6, 18),
-    name_func=lambda f: 'Division %s' % f.get('ElectoralID'),
+    name_func=lambda f: 'Division %s' % f.get('ElectoralD'),
     id_func=boundaries.attr('ElectoralID'),
     authority='County of Grande Prairie No. 1',
     source_url='http://data.countygp.ab.ca/',
