0877d6e31c2a943caab614c4c8517558cbf16da9
James McKinney <james@slashpoundbang.com>
2012-08-17 16:18:44 -0400

    add st john's

---
+++ nl/stjohns/ed/definition.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register("St. John's wards",
+    domain="St. John's, NL",
+    last_updated=date(2012, 8, 17),
+    name_func=boundaries.attr('WARDNAME'),
+    id_func=boundaries.attr('WARD'),
+    authority="City of St. John's",
+    notes='We use a shapefile received via email.',
+    encoding='iso-8859-1',
+)
