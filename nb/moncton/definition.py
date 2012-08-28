395388104548bd3047f33badba2ac4dcbb6a8896
James McKinney <james@slashpoundbang.com>
2012-08-28 01:36:12 -0400

    fix file modes

--- nb/moncton/definition.py
+++ nb/moncton/definition.py
@@ -3,7 +3,7 @@ from datetime import date
 import boundaries
 
 boundaries.register('Moncton wards',
-    domain='Moncton, NB,
+    domain='Moncton, NB',
     last_updated=date(2012, 8, 21),
     name_func=lambda f: 'Ward %s' % f.get('WARDNUMB'),
     id_func=boundaries.attr('WARDNUMB'),
