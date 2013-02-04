367b46e3d3a4f49bd90993356f7616d407802e3c
Courtney Claessens <claessens.c@gmail.com>
2013-02-04 15:04:42 -0500

    added montreal districts

---
+++ qc/montreal/districts/definition.py
@@ -0,0 +1,241 @@
+#coding: utf-8
+from datetime import date
+
+import boundaries
+
+boundaries.register(u'Ahuntsic-Cartierville, Montréal, QC',
+    domain=u'Ahuntsic-Cartierville, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Anjou, Montréal, QC',
+    domain=u'Anjou, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Côte-des-Neiges - Notre-Dame-de-Grâce, Montréal, QC',
+    domain=u'Côte-des-Neiges - Notre-Dame-de-Grâce, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Lachine, Montréal, QC',
+    domain=u'Lachine, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'LaSalle, Montréal, QC',
+    domain=u'LaSalle, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Le Sud-Ouest, Montréal, QC',
+    domain=u'Le Sud-Ouest, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'L\'Île-Bizard–Sainte-Geneviève, Montréal, QC',
+    domain=u'L\'Île-Bizard–Sainte-Geneviève, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Mercier-Hochelaga-Maisonneuve, Montréal, QC',
+    domain=u'Mercier-Hochelaga-Maisonneuve, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Montréal Nord, Montréal, QC',
+    domain=u'Montréal Nord, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Outremont, Montréal, QC',
+    domain=u'Outremont, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Pierrefonds-Roxboro, Montréal, QC',
+    domain=u'Pierrefonds-Roxboro, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Le Plateau-Montréal, Montréal, QC',
+    domain=u'Le Plateau-Montréal, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Rivière-des-Prairies - Pointe-aux-Trembles, Montréal, QC',
+    domain=u'Rivière-des-Prairies - Pointe-aux-Trembles, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Rosemont - La-Petite-Patrie, Montréal, QC',
+    domain=u'Rosemont - La-Petite-Patrie, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Saint-Laurent, Montréal, QC',
+    domain=u'Saint-Laurent, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Saint-Léonard, Montréal, QC',
+    domain=u'Saint-Leonard, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Verdun, Montréal, QC',
+    domain=u'Verdun, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Ville-Marie, Montréal, QC',
+    domain=u'Ville-Marie, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+boundaries.register(u'Villeray - Saint-Michel - Parc-Extension, Montréal, QC',
+    domain=u'Villeray - Saint-Michel - Parc-Extension, Montréal, QC',
+    last_updated=date(2013, 4, 2),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/polygones-arrondissements/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    notes='District extracted from original shapefile',
+    encoding='iso-8859-1',
+)
+
+
+
+
+
+
+
+
+
