6e6d89d504b2bf8654aad3e24e0e57eabe8bd226
Ellie Marshall <ellie@opennorth.ca>
2013-06-25 17:46:49 -0400

    updated defn

--- bc/ed/definition.py
+++ bc/ed/definition.py
@@ -4,11 +4,11 @@ import boundaries
 
 boundaries.register('British Columbia electoral districts',
     domain='British Columbia',
-    last_updated=date(2011, 12, 12),
+    last_updated=date(2013, 06, 25),
     name_func=boundaries.attr('edname'),
     id_func=boundaries.attr('edabbr'),
     authority='Elections BC',
-    source_url='http://www.elections.bc.ca/index.php/voting/electoral-maps-profiles/geographic-information-system-spatial-data-files-2011/',
-    data_url='http://www.elections.bc.ca/docs/map/redis11/GIS/ED_Province.exe',
+    source_url='http://www3.elections.bc.ca/docs/map/redis12/GIS/Electoral%20District%20Boundaries.zip'
+    data_url='http://www3.elections.bc.ca/docs/map/redis12/GIS/Electoral%20District%20Boundaries.zip',
     encoding='iso-8859-1',
 )
