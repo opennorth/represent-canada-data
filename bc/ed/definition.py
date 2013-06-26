71f82d4ed008e67c17be5bff2ac4d25b469414af
James McKinney <james@slashpoundbang.com>
2013-06-26 13:55:09 -0400

    fix syntax error

--- bc/ed/definition.py
+++ bc/ed/definition.py
@@ -8,7 +8,7 @@ boundaries.register('British Columbia electoral districts',
     name_func=boundaries.attr('edname'),
     id_func=boundaries.attr('edabbr'),
     authority='Elections BC',
-    source_url='http://www3.elections.bc.ca/docs/map/redis12/GIS/Electoral%20District%20Boundaries.zip'
+    source_url='http://www3.elections.bc.ca/docs/map/redis12/GIS/Electoral%20District%20Boundaries.zip',
     data_url='http://www3.elections.bc.ca/docs/map/redis12/GIS/Electoral%20District%20Boundaries.zip',
     encoding='iso-8859-1',
 )
