57842079880c40a6d15dda52d9b10c83d2e292b7
James McKinney <james@slashpoundbang.com>
2013-08-09 01:33:52 -0400

    update saskatoon

--- sk/saskatoon/ed/definition.py
+++ sk/saskatoon/ed/definition.py
@@ -4,11 +4,13 @@ import boundaries
 
 boundaries.register('Saskatoon wards',
     domain='Saskatoon, SK',
-    last_updated=date(2012, 5, 14),
-    name_func=lambda f: 'Ward %s' % f.get('Ward'),
-    id_func=boundaries.attr('Ward'),
+    last_updated=date(2013, 4, 10),
+    name_func=lambda f: 'Ward %s' % f.get('Name'),
+    id_func=boundaries.attr('Name'),
     authority='City of Saskatoon',
-    notes='Ward 1 is split into two features. We merge them using Quantum GIS.',
+    source_url='http://opendata-saskatoon.cloudapp.net/DataBrowser/SaskatoonOpenDataCatalogueBeta/CurrentWardBoundaries',
+    data_url='https://saskatoonopendatastorage.blob.core.windows.net/converteddata/kml.CurrentWardBoundaries.zip',
+    licence_url='http://opendata-saskatoon.cloudapp.net/TermsOfUse/TermsOfUse',
     encoding='iso-8859-1',
     geographic_code='4711066',
 )
