1009f7b3a56c6d3ebc4b5232b865902f3a382b00
James McKinney <james@slashpoundbang.com>
2013-08-20 19:46:59 -0400

    remove a note

--- fed/csd/definition.py
+++ fed/csd/definition.py
@@ -6,12 +6,11 @@ boundaries.register('Census subdivisions',
     domain='Canada',
     last_updated=date(2011, 11, 28),
     name_func=boundaries.attr('CSDNAME'),
-    id_func=boundaries.attr('CSDUID'),
+    id_func=boundaries.attr('CSDUID'), # Census subdivision names are not unique across provinces and territories
     slug_func=boundaries.attr('CSDUID'),
     authority='Statistics Canada',
     source_url='http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/bound-limit-eng.cfm',
     licence_url='http://data.gc.ca/eng/open-government-licence-canada',
     data_url='http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gcsd000a11a_e.zip',
-    notes='Census subdivision names are not unique across provinces.',
     encoding='iso-8859-1',
 )
