cac50fd85c557e89c0347a0341a1cff2dd772d98
James McKinney <james@slashpoundbang.com>
2013-08-21 18:50:39 -0400

    update calgary license

--- ab/calgary/ed/definition.py
+++ ab/calgary/ed/definition.py
@@ -9,7 +9,7 @@ boundaries.register('Calgary wards',
     id_func=boundaries.attr('WARD_NUM'),
     authority='City of Calgary',
     source_url='https://cityonline.calgary.ca/Pages/Category.aspx?cat=CITYonlineDefault&category=PDCAdministrativeBoundaries&publicdata',
-    licence_url='https://cityonline.calgary.ca/Pages/Licensehandler.ashx',
+    licence_url='https://cityonline.calgary.ca/Pages/PdcTermsOfUse.aspx',
     encoding='iso-8859-1',
     metadata={'geographic_code': '4806016'},
 )
