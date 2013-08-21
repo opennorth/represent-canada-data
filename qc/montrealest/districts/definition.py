68baeef2f210c83ceed384fc02f773142412df6d
James McKinney <james@slashpoundbang.com>
2013-08-21 13:02:53 -0400

    make it possible to automatically update island-of-montreal municipalities

---
+++ qc/montrealest/districts/definition.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+from datetime import date
+
+import boundaries
+
+boundaries.register(u'Montréal-Est districts',
+    domain=u'Montréal-Est, QC',
+    last_updated=date(2013, 8, 21),
+    name_func=boundaries.clean_attr('NOM_DISTRI'),
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://depot.ville.montreal.qc.ca/elections-2009-districts/multi-poly/data.zip',
+    encoding='iso-8859-1',
+    metadata={'geographic_code': '2466007'},
+    ogr2ogr='''-where "MONTREAL='0'" -where "NUM_ARR='74'"''',
+)
--- qc/municipalities/README.txt
+++
@@ -1,2 +0,0 @@
-Source : Ville de Montréal
-URL : http://donnees.ville.montreal.qc.ca/fiche/elections-2009-districts/
