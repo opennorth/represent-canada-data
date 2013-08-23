b92b21c15d331c40400b3fd73c47a19b70f2dc34
James McKinney <james@slashpoundbang.com>
2013-08-22 23:47:05 -0400

    fix authority

--- fed/csd/definition.py
+++ fed/csd/definition.py
@@ -8,7 +8,7 @@ boundaries.register('Census subdivisions',
     name_func=boundaries.attr('CSDNAME'),
     id_func=boundaries.attr('CSDUID'), # Census subdivision names are not unique across provinces and territories
     slug_func=boundaries.attr('CSDUID'),
-    authority='Statistics Canada',
+    authority='Her Majesty the Queen in right of Canada',
     source_url='http://data.gc.ca/data/en/dataset/8b577e89-8c56-4048-b4db-b9d5c753d419',
     licence_url='http://data.gc.ca/eng/open-government-licence-canada',
     data_url='http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gcsd000a11a_e.zip',
