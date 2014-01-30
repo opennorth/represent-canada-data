4e699e214d66fd3345a7e1ef584799bbab9c9111
James McKinney <james@slashpoundbang.com>
2014-01-30 01:35:02 -0500

    fix typo in stratford ward

--- pe/stratford/ed/definition.py
+++ pe/stratford/ed/definition.py
@@ -3,10 +3,17 @@ import re
 
 import boundaries
 
+def namer(f):
+    import boundaries
+    n = boundaries.attr('NAME')(f)
+    if n == "Kellys Cove":
+        return "Kelly's Cove"
+    return n
+
 boundaries.register('Stratford wards',
     domain='Stratford, PE',
     last_updated=date(2013, 7, 19),
-    name_func=boundaries.attr('NAME'),
+    name_func=namer,
     id_func=lambda f: re.sub(r'\D', '', f.get('KEY')),
     authority='Elections Prince Edward Island',
     source_url='http://www.electionspei.ca/municipalities/stratford',
