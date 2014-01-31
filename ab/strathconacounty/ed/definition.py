eff18a38bbebfbe5054d2b12b682a36d996e8920
James McKinney <james@slashpoundbang.com>
2014-01-31 17:42:45 -0500

    get strathcona from online source

--- ab/strathconacounty/ed/definition.py
+++ ab/strathconacounty/ed/definition.py
@@ -4,10 +4,13 @@ import boundaries
 
 boundaries.register('Strathcona County wards',
     domain='Strathcona County, AB',
-    last_updated=date(2012, 10, 11),
-    name_func=lambda f: 'Ward %d' % f.get('Ward'),
-    id_func=lambda f: '%d' % f.get('Ward'),
+    last_updated=date(2013, 10, 16),
+    name_func=boundaries.attr('Name'),
+    id_func=lambda f: re.sub('\D', '', f.get('Name')),
     authority='Strathcona County',
+    source_url='http://www.strathcona.ca/local-government/strathcona-county-elections/election-2013-open-data/#ward',
+    licence_url='http://www.strathcona.ca/local-government/strathcona-county-elections/election-2013-open-data/#licence',
+    data_url='http://www.strathcona.ca/files/files/at-lls-2013election-ward_boundaries.kmz',
     encoding='iso-8859-1',
     metadata={'geographic_code': '4811052'},
 )
