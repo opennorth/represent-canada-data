a38d135da9b4c0541458f9c1a33351209c8d811e
James McKinney <james@slashpoundbang.com>
2014-01-31 18:52:06 -0500

    automatically create borough district files

---
+++ qc/montreal/districts/rivieredesprairiespointeauxtrembles/definition.py
@@ -0,0 +1,17 @@
+#coding: utf-8
+from datetime import date
+
+import boundaries
+
+boundaries.register(u'Rivière-des-Prairies—Pointe-aux-Trembles districts',
+    domain=u'Rivière-des-Prairies—Pointe-aux-Trembles, Montréal, QC',
+    last_updated=date(2013, 10, 16),
+    name_func=lambda f: re.sub(u'', '—', f.get('NOM_DISTRI')), # control character, m-dash
+    authority=u'Ville de Montréal',
+    source_url='http://donnees.ville.montreal.qc.ca/dataset/elections-2013-districts-electoraux',
+    licence_url='http://donnees.ville.montreal.qc.ca/licence/licence-texte-complet/',
+    data_url='http://donnees.ville.montreal.qc.ca/storage/f/2013-10-16T14%3A16%3A09.092Z/districtelect.zip',
+    encoding='iso-8859-1',
+    metadata={'ocd_division': u'ocd-division/country:ca/csd:2466023/arrondissement:rivière-des-prairies-pointe-aux-trembles'},
+    ogr2ogr=u'''-where "ARRONDISSE='Rivière-des-Prairies-Pointe-aux-Trembles'"''',
+)
