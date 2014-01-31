060bcc73441582b8450cef5debcfe0d5bd7b1c80
James McKinney <james@slashpoundbang.com>
2014-01-31 17:27:23 -0500

    add milton

---
+++ on/milton/ed/definition.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register('Milton wards',
+    domain='Milton, ON',
+    last_updated=date(2014, 1, 31),
+    name_func=lambda f: 'Ward %s' % re.search('WARD (\d+)', f.get('Descriptio')).group(1),
+    id_func=lambda f: re.search('WARD (\d+)', f.get('Descriptio')).group(1),
+    authority='Town of Milton',
+    source_url='http://icreateopendata.public.esolutionsgroup.ca/home/details/1a3d6722-a3a9-4591-abc8-ac55c060a65a',
+    licence_url='http://www.milton.ca/en/resourcesGeneral/Open_Data/Milton_Open_Data_Terms_V1.pdf',
+    data_url='http://icreateopendata.public.esolutionsgroup.ca/home/ServeFile/1a3d6722-a3a9-4591-abc8-ac55c060a65a?FileType=6',
+    encoding='iso-8859-1',
+    metadata={'geographic_code': '3524009'},
+)
