6b0cb04151d8777bbb92b28691a928c28c7e6cd9
James McKinney <james@slashpoundbang.com>
2014-01-31 17:21:51 -0500

    add hamilton

---
+++ on/hamilton/ed/definition.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register('Hamilton wards',
+    domain='Hamilton, ON',
+    last_updated=date(2013, 11, 4),
+    name_func=lambda f: 'Ward %s' % f.get('WARD'),
+    id_func=boundaries.attr('WARD'),
+    authority='City of Hamilton',
+    source_url='http://www.hamilton.ca/ProjectsInitiatives/OpenData/',
+    licence_url='http://www.hamilton.ca/NR/rdonlyres/C58984A4-FE11-40B9-A231-8572EB922AAA/0/OpenDataTermsAndConditions_Final.htm',
+    data_url='http://opendata.hamilton.ca/SHP/Wards.zip',
+    encoding='iso-8859-1',
+    metadata={'geographic_code': '3525005'},
+)
