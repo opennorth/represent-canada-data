bb6922a131c69ea823d4f0465bc116ae34d2d219
James McKinney <james@slashpoundbang.com>
2014-02-04 13:23:13 -0500

    fix ottawa data url

--- on/ottawa/ed/definition.py
+++ on/ottawa/ed/definition.py
@@ -10,7 +10,7 @@ boundaries.register('Ottawa wards',
     authority='City of Ottawa',
     source_url='http://data.ottawa.ca/en/dataset/wards-2010',
     licence_url='http://ottawa.ca/en/mobile-apps-and-open-data/open-data-terms-use',
-    data_url='http://data.ottawa.ca/en/dataset/wards-2010/resource/6db490f2-f1db-4c7b-bf5a-cc0826a98776',
+    data_url='http://data.ottawa.ca/en/storage/f/2013-05-15T145937/Wards-2010-SHP.zip',
     encoding='iso-8859-1',
     metadata={'geographic_code': '3506008'},
 )
