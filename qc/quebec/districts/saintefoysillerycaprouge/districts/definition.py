0f8111bc9d264c5751f4fa9726c9307cabcd3e92
James McKinney <james@slashpoundbang.com>
2014-03-01 13:32:47 -0500

    add quebec districts

---
+++ qc/quebec/districts/saintefoysillerycaprouge/districts/definition.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from datetime import date
+
+import boundaries
+
+boundaries.register(u'Sainte-Foy–Sillery–Cap-Rouge districts',
+    domain=u'Sainte-Foy–Sillery–Cap-Rouge, Québec, QC',
+    last_updated=date(2014, 3, 1),
+    name_func=boundaries.attr('NOM'),
+    id_func=boundaries.attr('CODE'),
+    authority=u'Ville de Québec',
+    source_url='http://donnees.ville.quebec.qc.ca/donne_details.aspx?jdid=2',
+    licence_url='https://creativecommons.org/licenses/by/4.0/legalcode',
+    data_url='http://donnees.ville.quebec.qc.ca/Handler.ashx?id=43&f=SHP',
+    encoding='iso-8859-1',
+    metadata={'ocd_division': u'ocd-division/country:ca/csd:2423027/borough:sainte-foy~sillery~cap-rouge'},
+    ogr2ogr='''-where "DATE_FIN='2017/11/05' AND ARROND='Sainte-FoySilleryCap-Rouge'"''',
+)
