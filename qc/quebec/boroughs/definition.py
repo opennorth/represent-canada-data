20cca22ca1a40cfde4e735ed4bc1633801021a9b
James McKinney <james@slashpoundbang.com>
2014-03-01 11:49:47 -0500

    update quebec borough definition

--- qc/quebec/boroughs/definition.py
+++ qc/quebec/boroughs/definition.py
@@ -5,14 +5,13 @@ import boundaries
 
 boundaries.register(u'Québec boroughs',
     domain=u'Québec, QC',
-    last_updated=date(2014, 2, 28),
+    last_updated=date(2014, 3, 1),
     name_func=lambda f: f.get('NOM').replace('?', u'—'),  # m-dash
     id_func=boundaries.attr('CODE'),
     authority=u'Ville de Québec',
     source_url='http://donnees.ville.quebec.qc.ca/donne_details.aspx?jdid=2',
-    licence_url='http://donnees.ville.quebec.qc.ca/licence.aspx',
+    licence_url='https://creativecommons.org/licenses/by/4.0/legalcode',
     data_url='http://donnees.ville.quebec.qc.ca/Handler.ashx?id=2&f=SHP',
     encoding='iso-8859-1',
     metadata={'geographic_code': '2423027'},
-    prj='http://spatialreference.org/ref/epsg/4326/prj/',
 )
