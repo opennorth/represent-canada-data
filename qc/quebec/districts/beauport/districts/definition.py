c2560abe9d53d3c35c04c61b32562b3d3f185194
James McKinney <james@slashpoundbang.com>
2014-03-04 17:57:19 -0500

    add invisible character

--- qc/quebec/districts/beauport/districts/definition.py
+++ qc/quebec/districts/beauport/districts/definition.py
@@ -6,7 +6,7 @@ import boundaries
 boundaries.register(u'Beauport districts',
     domain=u'Beauport, Québec, QC',
     last_updated=date(2014, 3, 1),
-    name_func=lambda f: re.sub(u'', u'—', f.get('NOM')), # unknown character, m-dash
+    name_func=lambda f: re.sub(u'', u'—', f.get('NOM')), # unknown character, m-dash
     id_func=boundaries.attr('CODE'),
     authority=u'Ville de Québec',
     source_url='http://donnees.ville.quebec.qc.ca/donne_details.aspx?jdid=2',
