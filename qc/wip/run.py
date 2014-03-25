702ae819bea8f5414048d1060f75f6bae1b2e994
James McKinney <james@slashpoundbang.com>
2014-03-25 18:15:43 -0400

    merge indian reserves and settlements into municipalities

--- qc/wip/run.py
+++ qc/wip/run.py
@@ -2,6 +2,7 @@
 import os.path
 
 from invoke import run
+from unidecode import unidecode
 
 # Creates a shapefile for each Indian reserve and Indian settlement to be merged
 # into its parent municipality's shapefile.
@@ -22,6 +23,6 @@ sets = {
 }
 
 for geographic_code, name in sets.items():
-  file = 'qc/wip/%s-24%s.shp' % (name, geographic_code)
+  file = 'qc/wip/%s-24%s.shp' % (unidecode(name), geographic_code)
   if not os.path.exists(file):
     run('''ogr2ogr -f "ESRI Shapefile" "%s" "qc/districts/Districts Elec Mun 2014-02-28_DetU_region.shp" -where "CO_MUNCP='%s'"''' % (file, geographic_code), echo=True)
