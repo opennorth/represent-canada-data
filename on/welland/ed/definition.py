864573621730ae6d3876dfcb54ee35dab77e80a7
James McKinney <james@slashpoundbang.com>
2014-08-06 17:49:09 -0700

    fix broken urls

--- on/welland/ed/definition.py
+++ on/welland/ed/definition.py
@@ -4,7 +4,7 @@ import boundaries
 
 boundaries.register('Welland wards',
     domain='Welland, ON',
-    last_updated=date(2014, 4, 7),
+    last_updated=date(2014, 7, 1),
     name_func=lambda f: 'Ward %s' % f.get('WARD'),
     id_func=boundaries.attr('WARD'),
     authority='City of Welland',
--- sk/regina/ed/definition.py
+++ sk/regina/ed/definition.py
@@ -4,7 +4,7 @@ import boundaries
 
 boundaries.register('Regina wards',
     domain='Regina, SK',
-    last_updated=date(2014, 1, 28),
+    last_updated=date(2014, 4, 17),
     name_func=lambda f: 'Ward %s' % f.get('WARD'),
     id_func=boundaries.attr('WARD'),
     authority='City of Regina',
