2ab27f71a92ff0adf70d3e078fcb8ff6da41e5c4
James McKinney <james@slashpoundbang.com>
2014-08-08 09:13:48 -0700

    use chathamkent open data

---
+++ on/chathamkent/ed/definition.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register('Chatham-Kent wards',
+    domain='Chatham-Kent, ON',
+    last_updated=date(2013, 10, 8),
+    name_func=boundaries.attr('WardName'),
+    id_func=boundaries.attr('WardNum'),
+    authority='Municipality of Chatham-Kent',
+    source_url='http://www.chatham-kent.ca/sp_training/OpenData/Pages/DataCatalogue.aspx',
+    data_url='http://www.chatham-kent.ca/sp_training/OpenData/Documents/Wards.shp.zip',
+    encoding='iso-8859-1',
+    metadata={'geographic_code': '3536020'},
+)
