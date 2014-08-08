49dd24080cb56da6f38bdadd67722896f292c7f9
James McKinney <james@slashpoundbang.com>
2014-08-08 09:24:40 -0700

    update kingston note

--- on/kingston/ed/definition.py
+++ on/kingston/ed/definition.py
@@ -7,7 +7,8 @@ boundaries.register('Kingston wards',
     last_updated=date(2012, 10, 25),
     name_func=boundaries.attr('ELECTORAL_'),
     id_func=boundaries.attr('ELECTORAL1'),
-    # @note The new data license is more restrictive than our current custom agreement.
+    # @note The new data license is more restrictive than our current custom
+    #   agreement. However, the new data is for 2014 boundaries.
     # source_url='http://www.cityofkingston.ca/explore/data-catalogue',
     # licence_url='http://www.cityofkingston.ca/documents/10180/144997/GIS+-+Data+License+Agreement+Template.pdf/45503bd1-500f-4cfd-af6f-f34221ce78c3',
     authority='City of Kingston',
