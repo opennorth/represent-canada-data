ab1d5a3f51b6169e15cf5dcaa1e2fdfaf00b173c
James McKinney <james@slashpoundbang.com>
2014-09-12 19:19:17 -0400

    switch to new nb boundaries

--- nb/ed/definition.py
+++ nb/ed/definition.py
@@ -4,16 +4,13 @@ import boundaries
 
 boundaries.register('New Brunswick electoral districts',
     domain='New Brunswick',
-    last_updated=date(2011, 3, 8),
+    last_updated=date(2014, 8, 26),
     name_func=boundaries.attr('PED_Name_E'),
-    # DIST_ID
-    id_func=boundaries.attr('PED_Num'),
+    id_func=boundaries.attr('DIST_ID'),
     authority='Her Majesty the Queen in Right of New Brunswick',
-    source_url='http://www.gnb.ca/elections/10prov/10provmap-e.asp',
-    data_url='http://www.gnb.ca/elections/pdf/2010PEDMaps/NB_Electoral_Districts.zip',
-    # source_url='http://www.snb.ca/geonb1/e/DC/catalogue-E.asp',
-    # data_url='http://geonb.snb.ca/downloads/prov_electoral_districts/geonb_2014_ped-cep_shp.zip',
-    # licence_url='http://geonb.snb.ca/downloads/documents/geonb_license_e.pdf',
+    source_url='http://www.snb.ca/geonb1/e/DC/catalogue-E.asp',
+    data_url='http://geonb.snb.ca/downloads/prov_electoral_districts/geonb_2014_ped-cep_shp.zip',
+    licence_url='http://geonb.snb.ca/downloads/documents/geonb_license_e.pdf',
     encoding='iso-8859-1',
     metadata={'geographic_code': '13'},
 )
---
+++ nb/ed/read_me.txt
@@ -0,0 +1,26 @@
+Service New Brunswick - GeoNB
+http://www.snb.ca/geonb
+
+**DESCRIPTION**
+This dataset represents the 2014 electoral districts of New Brunswick as polygons. Each district is represented by one Member of the Legislative Assembly (MLA). Attributes: District names, District Number, Poll numbers, MLA Names.
+
+**FORMAT**
+ESRI Shape File
+
+**FILE LISTING**
+geonb_2014_ped-cep.* - graphics and attributes
+
+**SPATIAL FRAMEWORK**
+Datum: North American Datum 1983 (CSRS)
+Map Projection: NB Stereographic Double 
+EPSG code: 2953
+
+**SOURCE**
+Elections NB
+
+**DATE**
+2014-08-25
+
+**LICENSE**
+GeoNB License
+http://geonb.snb.ca/downloads/documents/geonb_license_e.pdf
