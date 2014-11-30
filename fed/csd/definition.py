9fb627e3202f850dc77104c2acc281660b21bf19
James McKinney <james@slashpoundbang.com>
2014-11-29 22:53:14 -0500

    update fed source url

--- fed/csd/definition.py
+++ fed/csd/definition.py
@@ -11,7 +11,7 @@ boundaries.register('Census subdivisions',
     id_func=boundaries.attr('CSDUID'),  # Census subdivision names are not unique across provinces and territories
     slug_func=boundaries.attr('CSDUID'),
     authority='Her Majesty the Queen in Right of Canada',
-    source_url='http://data.gc.ca/data/en/dataset/8b577e89-8c56-4048-b4db-b9d5c753d419',
+    source_url='http://open.canada.ca/data/en/dataset/8b577e89-8c56-4048-b4db-b9d5c753d419',
     licence_url='http://open.canada.ca/en/open-government-licence-canada',
     data_url='http://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/gcsd000a11a_e.zip',
     encoding='iso-8859-1',
