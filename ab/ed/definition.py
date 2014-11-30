407fc6c875507edbf976410c8da1c393adf02d5e
James McKinney <james@slashpoundbang.com>
2014-11-29 23:12:35 -0500

    comment out ab data url

--- ab/ed/definition.py
+++ ab/ed/definition.py
@@ -12,7 +12,8 @@ boundaries.register('Alberta electoral districts',
     authority='Her Majesty the Queen in Right of Alberta',
     source_url='http://www.altalis.com/products/base/20k_base_features.html',
     licence_url='http://www.altalis.com/pdf/AltaLIS%20Web%20Use%20Agreement.pdf',
-    data_url='http://data.altalis.com/AltalisDataDownload/Download/525711F29A214C1AB5ED8F5578A983C3',
+    # The data_url contains multiple shapefiles.
+    # data_url='http://data.altalis.com/AltalisDataDownload/Download/525711F29A214C1AB5ED8F5578A983C3',
     encoding='iso-8859-1',
     metadata={'geographic_code': '48'},
 )
