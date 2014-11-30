1b7ae1cf7502b062875c8e191df61b86f16f4ee6
James McKinney <james@slashpoundbang.com>
2014-11-29 23:34:54 -0500

    update ns halifax data

--- ns/halifax/ed/definition.py
+++ ns/halifax/ed/definition.py
@@ -6,13 +6,13 @@ import boundaries
 
 boundaries.register('Halifax districts',
     domain='Halifax, NS',
-    last_updated=date(2014, 9, 12),
+    last_updated=date(2014, 11, 30),
     name_func=boundaries.attr('DISTNAME'),
     id_func=boundaries.attr('DIST_ID'),
     authority='Halifax Regional Municipality',
     source_url='http://catalogue.hrm.opendata.arcgis.com/datasets/b2c71fb0d90f41488fae2ad2c8278f6d_0',
     data_url='http://catalogue.hrm.opendata.arcgis.com/datasets/b2c71fb0d90f41488fae2ad2c8278f6d_0.zip',
     licence_url='http://www.halifax.ca/opendata/OD_TermsOfUse.php',
-    encoding='iso-8859-1',
+    encoding='utf-8',
     metadata={'geographic_code': '1209034'},
 )
