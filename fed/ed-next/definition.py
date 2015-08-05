af247c4e2a01ba5e1f03b188d79df548da8e6d8b
James McKinney <james@slashpoundbang.com>
2015-08-05 17:00:15 -0400

    ed-next: fix riding name

--- fed/ed-next/definition.py
+++ fed/ed-next/definition.py
@@ -7,48 +7,7 @@ import boundaries
 
 
 def namer(f):
-    import boundaries
-    n = boundaries.clean_attr('ENNAME')(f).replace('\x97', '—')
-
-    # @see http://www.parl.gc.ca/HousePublications/Publication.aspx?Language=E&Mode=1&DocId=6684609&File=4
-    mappings = {
-        "Western Arctic": "Northwest Territories",
-        # "Blainville": "Thérèse-De Blainville",
-        # "Boucher—Les Patriotes—Verchères": "Pierre-Boucher—Les Patriotes—Verchères",
-        # "Centre-du-Bas-Saint-Laurent": "Rimouski-Neigette—Témiscouata—Les Basques",
-        # "Charlevoix—Montmorency": "Beauport—Côte-de-Beaupré—Île d’Orléans—Charlevoix",
-        # "Chicoutimi": "Chicoutimi—Le Fjord",
-        # "Dorval—Lachine": "Dorval—Lachine—LaSalle",
-        # "LaSalle—Verdun": "LaSalle—Émard—Verdun",
-        # "LeMoyne": "Longueuil—Charles-LeMoyne",
-        # "Longueuil": "Longueuil—Saint-Hubert",
-        # "Mont-Royal": "Mount Royal",
-        # "Sainte-Rose": "Marc-Aurèle-Fortin",
-        # "Soulanges—Vaudreuil": "Vaudreuil—Soulanges",
-        # "Ville-Marie": "Ville-Marie—Le Sud-Ouest—Île-des-Soeurs",
-        # "Brant": "Brantford—Brant",
-        # "Lanark—Frontenac": "Lanark—Frontenac—Kingston",
-        # "Leeds—Grenville": "Leeds—Grenville—Thousand Islands and Rideau Lakes",
-        # "Mississauga—Cooksville": "Mississauga East—Cooksville",
-        # "Northumberland—Pine Ridge": "Northumberland—Peterborough South",
-        # "Ottawa—Orléans": "Orléans",
-        # "Peterborough": "Peterborough—Kawartha",
-        # "Rideau—Carleton": "Carleton",
-        # "St. Paul's": "Toronto—St. Paul's",
-        # "York West": "Humber River—Black Creek",
-        # "Humboldt—Warman—Martensville—Rosetown": "Carlton Trail—Eagle Creek",
-        # "Grande Prairie": "Grande Prairie—Mackenzie",
-        # "Medicine Hat": "Medicine Hat—Cardston—Warner",
-        # "Red Deer—Wolf Creek": "Red Deer—Lacombe",
-        # "Sturgeon River": "Sturgeon River—Parkland",
-        # "Saanich—Esquimalt—Juan de Fuca": "Esquimalt—Saanich—Sooke",
-        # "Vancouver Island North—Comox—Powell River": "North Island—Powell River",
-    }
-
-    for key, value in mappings.items():
-        if n == key:
-            return value
-    return n
+    return boundaries.clean_attr('ENNAME')(f).replace('\x97', '—').replace(' dOr', " d'Or")  # Beauport--Côte-de-Beaupré--Île dOrléans--Charlevoix
 
 boundaries.register('Federal electoral districts (next election)',
     singular='Federal electoral district',
