544a370d12507c96e883947b06bed94eb9183e3d
James McKinney <james@slashpoundbang.com>
2015-08-14 10:14:32 -0400

    update greatersudbury from web

--- on/greatersudbury/ed/definition.py
+++ on/greatersudbury/ed/definition.py
@@ -6,10 +6,14 @@ import boundaries
 
 boundaries.register('Greater Sudbury wards',
     domain='Greater Sudbury, ON',
-    last_updated=date(2012, 2, 21),
-    name_func=lambda f: 'Ward %s' % f.get('Ward'),
-    id_func=boundaries.attr('Ward'),
+    last_updated=date(2015, 8, 14),
+    name_func=lambda f: 'Ward %s' % f.get('WardNumber'),
+    id_func=boundaries.attr('WardNumber'),
     authority='City of Greater Sudbury',
+    source_url='http://data.sudbury.opendata.arcgis.com/datasets/2fd80bad151b42cea9a6ee103d0dbc3d_0',
+    licence_url='http://www.greatersudbury.ca/inside-city-hall/open-data/licence/',
+    data_url='http://data.sudbury.opendata.arcgis.com/datasets/2fd80bad151b42cea9a6ee103d0dbc3d_0.zip',
+    is_valid_func=lambda f: f.get('WardNumber'),
     encoding='iso-8859-1',
     metadata={'geographic_code': '3553005'},
 )
