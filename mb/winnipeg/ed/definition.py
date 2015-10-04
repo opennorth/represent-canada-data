4e01d4732fda3abcc79b0fe3083b22b25b08972a
James McKinney <james@slashpoundbang.com>
2015-10-04 18:04:23 -0400

    fix winnipeg ward names

--- mb/winnipeg/ed/definition.py
+++ mb/winnipeg/ed/definition.py
@@ -7,7 +7,7 @@ import boundaries
 boundaries.register('Winnipeg wards',
     domain='Winnipeg, MB',
     last_updated=date(2015, 10, 3),
-    name_func=boundaries.attr('Name'),
+    name_func=boundaries.clean_attr('Name'),
     id_func=lambda f: int(f.get('Number')),
     authority='City of Winnipeg',
     source_url='https://data.winnipeg.ca/Council-Services/Electoral-Ward/ede3-teb8',
