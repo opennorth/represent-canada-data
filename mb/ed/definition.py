9ea0e16deb8f12c33d21e6c33cc4b4dff54bbdb0
James McKinney <james@slashpoundbang.com>
2015-10-04 18:39:23 -0400

    fix mb names

--- mb/ed/definition.py
+++ mb/ed/definition.py
@@ -7,7 +7,7 @@ import boundaries
 boundaries.register('Manitoba electoral districts',
     domain='Manitoba',
     last_updated=date(2011, 12, 14),
-    name_func=boundaries.dashed_attr('ED'),
+    name_func=boundaries.attr('ED'),
     authority='Her Majesty the Queen in Right of Manitoba',
     source_url='http://represent:weakpass@mli2.gov.mb.ca/adminbnd/index.html',
     licence_url='http://mli2.gov.mb.ca/app/register/app/index.php',
