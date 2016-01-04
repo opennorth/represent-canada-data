1af5b1bc4ded2222efd975ad63a707b73ee3184e
James McKinney <james@slashpoundbang.com>
2016-01-04 01:13:11 -0500

    add 2012 sk boundaries

---
+++ boundaries/ocd-division/country:ca/province:sk/2012/definition.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from datetime import date
+
+import boundaries
+
+
+boundaries.register('Saskatchewan electoral districts (2012 Representation Order)',
+    singular='Saskatchewan electoral district',
+    domain='Saskatchewan',
+    last_updated=date(2014, 3, 20),
+    name_func=boundaries.dashed_attr('Con_Name'),
+    id_func=boundaries.attr('Con_Num'),
+    authority='Her Majesty the Queen in Right of Saskatchewan',
+    source_url='https://www.geosask.ca/',
+    licence_url='https://www.geosask.ca/Portal/jsp/terms_popup.jsp',
+    data_url='ftp://portaldata:freedata@ftp.isc.ca/PackagedData/ElectionsSask/2013_Constituency.zip',
+    encoding='iso-8859-1',
+    extra={'division_id': 'ocd-division/country:ca/province:sk'},
+)
