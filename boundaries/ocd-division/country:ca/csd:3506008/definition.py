3a33b5e31202a648d4cfe3212efd55e842f49277
James McKinney <james@slashpoundbang.com>
2016-01-08 11:35:45 -0500

    update ottawa source

--- boundaries/ocd-division/country:ca/csd:3506008/definition.py
+++ boundaries/ocd-division/country:ca/csd:3506008/definition.py
@@ -6,13 +6,13 @@ import boundaries
 
 boundaries.register('Ottawa wards',
     domain='Ottawa, ON',
-    last_updated=date(2015, 10, 6),
+    last_updated=date(2016, 1, 5),
     name_func=boundaries.clean_attr('DESCRIPTIO'),
     id_func=boundaries.attr('WARD_NUM'),
     authority='City of Ottawa',
-    source_url='http://data.ottawa.ca/en/dataset/wards-2010',
+    source_url='http://data.ottawa.ca/en/dataset/wards-2014',
     licence_url='http://ottawa.ca/en/mobile-apps-and-open-data/open-data-terms-use',
-    data_url='http://data.ottawa.ca/dataset/13deeed4-1cd5-4a68-a10d-9839d3677446/resource/6abe3aa1-4ad8-4061-bb18-dad5054b85fb/download/wards-2010-2.shp.zip',
+    data_url='http://data.ottawa.ca/dataset/8321248d-0b86-47cd-9c83-c848a1bc0098/resource/8152e707-fc28-409c-9bbe-8cb9f88dca86/download/wards-2014shp.shp.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3506008'},
 )
