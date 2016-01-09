8d8453ac91327b1bf1c02094560d0d29010d3695
James McKinney <james@slashpoundbang.com>
2016-01-09 02:13:11 -0500

    fix ottawa encoding

--- boundaries/ocd-division/country:ca/csd:3506008/definition.py
+++ boundaries/ocd-division/country:ca/csd:3506008/definition.py
@@ -13,6 +13,6 @@ boundaries.register('Ottawa wards',
     source_url='http://data.ottawa.ca/en/dataset/wards-2014',
     licence_url='http://ottawa.ca/en/mobile-apps-and-open-data/open-data-terms-use',
     data_url='http://data.ottawa.ca/dataset/8321248d-0b86-47cd-9c83-c848a1bc0098/resource/8152e707-fc28-409c-9bbe-8cb9f88dca86/download/wards-2014shp.shp.zip',
-    encoding='iso-8859-1',
+    encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/csd:3506008'},
 )
