47ebd82e9325cb66fcb83386489be5366636ef0e
James McKinney <james@slashpoundbang.com>
2016-01-09 02:11:22 -0500

    update ottawa

--- boundaries/ocd-division/country:ca/csd:3506008/definition.py
+++ boundaries/ocd-division/country:ca/csd:3506008/definition.py
@@ -1,21 +1,18 @@
 from __future__ import unicode_literals
 
-import re
 from datetime import date
 
 import boundaries
 
 boundaries.register('Ottawa wards',
     domain='Ottawa, ON',
-    last_updated=date(2016, 1, 5),
-    name_func=boundaries.attr('Name'),
-    id_func=lambda f: re.sub(r'\D', '', f.get('Name')),
+    last_updated=date(2016, 1, 8),
+    name_func=boundaries.clean_attr('WARD_EN'),
+    id_func=boundaries.attr('WARD_NUM'),
     authority='City of Ottawa',
     source_url='http://data.ottawa.ca/en/dataset/wards-2014',
     licence_url='http://ottawa.ca/en/mobile-apps-and-open-data/open-data-terms-use',
-    # Jan 8: Reported the shapefile as missing a DBF file.
-    # http://data.ottawa.ca/dataset/8321248d-0b86-47cd-9c83-c848a1bc0098/resource/8152e707-fc28-409c-9bbe-8cb9f88dca86/download/wards-2014shp.shp.zip
-    data_url='http://data.ottawa.ca/dataset/8321248d-0b86-47cd-9c83-c848a1bc0098/resource/d63e4443-febd-4d4a-ba9a-444d49c10124/download/wards-2014kmz.kmz',
+    data_url='http://data.ottawa.ca/dataset/8321248d-0b86-47cd-9c83-c848a1bc0098/resource/8152e707-fc28-409c-9bbe-8cb9f88dca86/download/wards-2014shp.shp.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3506008'},
 )
