a69fe5e50feb380f9a7c354d54fb80f97e097894
James McKinney <james@slashpoundbang.com>
2016-08-07 16:57:10 -0400

    Merge features in Argyle

--- boundaries/ca_ns_districts/definition.py
+++ boundaries/ca_ns_districts/definition.py
@@ -48,5 +48,5 @@ for fragment, (name, machine_name, type) in sets.items():
         extra={'division_id': 'ocd-division/country:ca/%s' % fragment},
         ogr2ogr='''-where "mu_code='%s'"''' % machine_name,
         base_file='geo_export_383c6711-c217-43ff-a2bc-2444da2b6425.shp',
-        notes='Merge electoral districts with multiple features into single features (GY02).',
+        notes='Merge electoral districts with multiple features into single features (AY01, GY02, SH01, SH04).',
     )
