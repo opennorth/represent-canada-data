6150d91378d4d3a1178ef03d7aed840854f3cae2
James McKinney <james@slashpoundbang.com>
2016-08-08 22:36:41 -0400

    ca_on_guelph: update

--- boundaries/ocd-division/country:ca/csd:3523008/definition.py
+++ boundaries/ocd-division/country:ca/csd:3523008/definition.py
@@ -6,14 +6,13 @@ import boundaries
 
 boundaries.register('Guelph wards',
     domain='Guelph, ON',
-    last_updated=date(2014, 2, 7),
-    name_func=boundaries.attr('NAME'),
+    last_updated=date(2016, 8, 8),
+    name_func=lambda f: 'Ward %s' % f.get('WARD'),
     id_func=boundaries.attr('WARD'),
     authority='City of Guelph',
     source_url='http://data.open.guelph.ca/dataset/guelph-wards',
-    # Shapefile was available and is now requested, because KML stores attributes as SimpleData.
-    data_url='http://data.open.guelph.ca/datafiles/gis/Wards.kml',
     licence_url='http://data.open.guelph.ca/pages/open-government-licence',
+    data_url='http://data.open.guelph.ca/datafiles/gis/GuelphWards.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3523008'},
 )
