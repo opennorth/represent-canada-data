6d5aea335a6c04b7734b1f0cd1396cfa17a2c718
James McKinney <james@slashpoundbang.com>
2016-11-23 18:26:41 -0500

    Update Chatham-Kent

--- boundaries/ocd-division/country:ca/csd:3536020/definition.py
+++ boundaries/ocd-division/country:ca/csd:3536020/definition.py
@@ -6,13 +6,13 @@ import boundaries
 
 boundaries.register('Chatham-Kent wards',
     domain='Chatham-Kent, ON',
-    last_updated=date(2013, 10, 8),
-    name_func=boundaries.attr('WardName'),
-    id_func=boundaries.attr('WardNum'),
+    last_updated=date(2016, 8, 12),
+    name_func=lambda f: 'Ward %s' % f.get('WARD_NO'),
+    id_func=boundaries.attr('WARD_NO'),
     authority='Municipality of Chatham-Kent',
-    source_url='http://chathamkentopendata.chatham-kent.opendata.arcgis.com/',
+    source_url='http://chathamkentopendata.chatham-kent.opendata.arcgis.com/datasets/1f4428dd5d764320b4246d190cfb70cb_0',
     licence_url='https://www.arcgis.com/sharing/rest/content/items/2ffb1ce148804fe4ade2414e6ef10d21/data',
-    data_url='http://www.chatham-kent.ca/sp_training/OpenData/Documents/Wards.shp.zip',
+    data_url='http://chathamkentopendata.chatham-kent.opendata.arcgis.com/datasets/1f4428dd5d764320b4246d190cfb70cb_0.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3536020'},
 )
