f3407520923fecf5449a21c08b54d2e75987205a
James McKinney <james@slashpoundbang.com>
2016-11-24 10:13:04 -0500

    Update King shp

--- boundaries/ocd-division/country:ca/csd:3519049/definition.py
+++ boundaries/ocd-division/country:ca/csd:3519049/definition.py
@@ -3,6 +3,7 @@ from datetime import date
 import boundaries
 
 boundaries.register('King wards',
+    file='King.shp',
     domain='King, ON',
     last_updated=date(2016, 11, 23),
     name_func=lambda f: 'Ward %s' % f.get('WARD_NUMBE'),
@@ -10,5 +11,6 @@ boundaries.register('King wards',
     authority='Township of King',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3519049'},
-    ogr2ogr='''-where "WARD_NUMBE>0"''',
+    ogr2ogr='''-where "WARD_NUMBE<>0"''',
+    base_file='ward.shp',
 )
