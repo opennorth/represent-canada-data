57cd2f8141383950cfb240fec3c3f669c859b129
James McKinney <james@slashpoundbang.com>
2016-11-24 09:53:56 -0500

    Add King

---
+++ boundaries/ocd-division/country:ca/csd:3519049/definition.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+import boundaries
+
+boundaries.register('King wards',
+    domain='King, ON',
+    last_updated=date(2016, 11, 23),
+    name_func=lambda f: 'Ward %s' % f.get('WARD_NUMBE'),
+    id_func=boundaries.attr('WARD_NUMBE'),
+    authority='Township of King',
+    encoding='iso-8859-1',
+    extra={'division_id': 'ocd-division/country:ca/csd:3519049'},
+    ogr2ogr='''-where "WARD_NUMBE<>'0'"''',
+)
