d30c2dc9ded9b11e5eb5cae0a28dad2e72c37c8f
James McKinney <james@slashpoundbang.com>
2017-01-17 09:25:16 -0500

    Add Norfolk County

---
+++ boundaries/ocd-division/country:ca/csd:3528052/definition.py
@@ -0,0 +1,14 @@
+import re
+from datetime import date
+
+import boundaries
+
+boundaries.register('Norfolk County wards',
+    domain='Norfolk County, ON',
+    last_updated=date(2017, 1, 16),
+    name_func=lambda f: 'Ward %s' % f.get('Ward'),
+    id_func=boundaries.attr('Ward'),
+    authority='Norfolk County',
+    encoding='iso-8859-1',
+    extra={'division_id': 'ocd-division/country:ca/csd:3528052'},
+)
