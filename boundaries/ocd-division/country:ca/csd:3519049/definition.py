ec993567d054a27f9674dd4fa2f47d9047efee0f
James McKinney <james@slashpoundbang.com>
2017-02-04 00:21:39 -0500

    Switch ogr2ogr to is_valid_func

--- boundaries/ocd-division/country:ca/csd:3519049/definition.py
+++ boundaries/ocd-division/country:ca/csd:3519049/definition.py
@@ -3,7 +3,6 @@ from datetime import date
 import boundaries
 
 boundaries.register('King wards',
-    file='King.shp',
     domain='King, ON',
     last_updated=date(2016, 11, 23),
     name_func=lambda f: 'Ward %s' % f.get('WARD_NUMBE'),
@@ -11,6 +10,5 @@ boundaries.register('King wards',
     authority='Township of King',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3519049'},
-    ogr2ogr='''-where "WARD_NUMBE<>0"''',
-    base_file='ward.shp',
+    is_valid_func=lambda f: int(f.get('WARD_NUMBE')) != 0,
 )
