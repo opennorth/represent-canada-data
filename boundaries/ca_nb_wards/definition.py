ef7347103ee1db21fdcc16584c715d580ea998c1
James McKinney <james@slashpoundbang.com>
2017-02-04 01:49:36 -0500

    Use is_valid_func instead of ogr2ogr for ca_nb_wards

--- boundaries/ca_nb_wards/definition.py
+++ boundaries/ca_nb_wards/definition.py
@@ -5,13 +5,16 @@ from datetime import date
 
 import boundaries
 
+# The shapefile contains features with MUN_NAME equal to Cocagne, Hanwell and
+# Tracadie, but these are Census places, not Census subdivisions.
 sets = {
     1301006: ['Saint John', 'Saint John'],
     1302004: ['Campobello Island', 'Campobello'],
     1303012: ['Oromocto', 'Oromocto'],
     1306020: ['Riverview', 'Riverview'],
     1307005: ['Beaubassin East', 'Beaubassin East\Beaubassin-Est'],
-    1307013: ['Memramcook', 'Memramcook'],
+    # Memramcook was present in 2014-03-24, not in 2017-02-04.
+    # 1307013: ['Memramcook', 'Memramcook'],
     1307022: ['Moncton', 'Moncton'],
     1307045: ['Dieppe', 'Dieppe'],
     1309027: ['Upper Miramichi', 'Upper Miramichi'],
@@ -20,6 +23,7 @@ sets = {
     1311027: ['Florenceville-Bristol', 'Florenceville-Bristol'],
     1313002: ['Saint-André', 'Saint-André'],
     1313027: ['Edmundston', 'Edmunston'],
+    1314013: ['Atholville', 'Atholville'],
     1314019: ['Kedgwick', 'Kedgwick'],
     1314025: ['Belledune', 'Belledune'],
     1315027: ['Bas-Caraquet', 'Bas-Caraquet'],
@@ -28,9 +32,8 @@ sets = {
 
 for geographic_code, (name, machine_name) in sets.items():
     boundaries.register('%s wards' % name,
-        file='%s.shp' % name,
         domain='%s, NB' % name,
-        last_updated=date(2014, 3, 24),
+        last_updated=date(2017, 2, 4),
         name_func=lambda f: 'Ward %s' % f.get('WARD_ID'),
         id_func=boundaries.attr('WARD_ID'),
         authority='Her Majesty the Queen in Right of New Brunswick',
@@ -38,12 +41,11 @@ for geographic_code, (name, machine_name) in sets.items():
         licence_url='http://geonb.snb.ca/downloads/documents/geonb_license_e.pdf',
         encoding='iso-8859-1',
         extra={'division_id': 'ocd-division/country:ca/csd:%d' % geographic_code},
-        ogr2ogr='''-where "MUN_NAME='%s'"''' % machine_name,
-        base_file='OGRGeoJSON.shp',
+        is_valid_func=lambda f, machine_name=machine_name: f.get('MUN_NAME') == machine_name,
+        notes='Compare the subdivisions in boundaries/ca_nb_wards/definition.py to `ogrinfo -al -geom=NO boundaries/ca_nb_wards | grep " MUN_NAME" | sort | uniq | cut -d= -f 2`.',
     )
 
 boundaries.register('Beresford wards',
-    file='Beresford.shp',
     domain='Beresford, NB',
     last_updated=date(2014, 3, 24),
     name_func=lambda f: 'Ward B' if f.get('OBJECTID') == 52 else 'Ward A',
@@ -52,12 +54,10 @@ boundaries.register('Beresford wards',
     licence_url='http://geonb.snb.ca/downloads/documents/geonb_license_e.pdf',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:1315015'},
-    ogr2ogr='''-where "MUN_NAME='Beresford'"''',
-    base_file='OGRGeoJSON.shp',
+    is_valid_func=lambda f: f.get('MUN_NAME') == 'Beresford',
 )
 
 boundaries.register('Grand Falls wards',
-    file='Grand Falls.shp',
     domain='Grand Falls, NB',
     last_updated=date(2014, 3, 24),
     name_func=lambda f: 'South Ward' if f.get('OBJECTID') == 35 else 'North Ward',
@@ -66,6 +66,5 @@ boundaries.register('Grand Falls wards',
     licence_url='http://geonb.snb.ca/downloads/documents/geonb_license_e.pdf',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:1312019'},
-    ogr2ogr='''-where "MUN_NAME='Grand Falls\Grand-Sault'"''',
-    base_file='OGRGeoJSON.shp',
+    is_valid_func=lambda f: f.get('MUN_NAME') == 'Grand Falls\Grand-Sault',
 )
