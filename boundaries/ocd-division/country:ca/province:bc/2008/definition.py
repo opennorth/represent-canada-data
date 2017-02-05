7475ffe51ad171bcc5c9c308068bd78753597914
James McKinney <james@slashpoundbang.com>
2017-02-04 20:50:34 -0500

    Update sources for province:bc

--- boundaries/ocd-division/country:ca/province:bc/2008/definition.py
+++ boundaries/ocd-division/country:ca/province:bc/2008/definition.py
@@ -4,14 +4,16 @@ from datetime import date
 
 import boundaries
 
-boundaries.register('British Columbia electoral districts',
+boundaries.register('British Columbia electoral districts',  # (2008 Redistribution)
+    singular='British Columbia electoral district',
     domain='British Columbia',
-    last_updated=date(2012, 9, 27),
-    name_func=boundaries.attr('edName'),
-    id_func=boundaries.attr('edAbbr'),
+    last_updated=date(2016, 11, 30),
+    name_func=boundaries.attr('ED_NAME'),
+    id_func=boundaries.attr('ED_ABBREV'),
     authority='Her Majesty the Queen in Right of British Columbia',
-    source_url='http://www.elections.bc.ca/index.php/maps/electoral-maps/geographic-information-system-spatial-data-files-2015/',
-    data_url='http://www3.elections.bc.ca/docs/map/redis12/GIS/Electoral%20District%20Boundaries.zip',
+    source_url='https://catalogue.data.gov.bc.ca/dataset/provincial-electoral-districts-electoral-boundaries-redistribution-2008',
+    licence_url='http://www.elections.bc.ca/docs/EBC-Open-Data-Licence.pdf',
+    data_url='https://catalogue.data.gov.bc.ca/dataset/c864f294-d302-4630-bde3-a0551735b309/resource/dc567bfb-488b-4765-b544-6bd02f61f736/download/edsre2008.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/province:bc'},
 )
---
+++ boundaries/ocd-division/country:ca/province:bc/2015/definition.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from datetime import date
+
+import boundaries
+
+boundaries.register('British Columbia electoral districts (2015 Redistribution)',
+    singular='British Columbia electoral district',
+    domain='British Columbia',
+    last_updated=date(2016, 11, 30),
+    name_func=boundaries.attr('ED_NAME'),
+    id_func=boundaries.attr('ED_ABBREV'),
+    authority='Her Majesty the Queen in Right of British Columbia',
+    source_url='https://catalogue.data.gov.bc.ca/dataset/provincial-electoral-districts-electoral-boundaries-redistribution-2015',
+    licence_url='http://www.elections.bc.ca/docs/EBC-Open-Data-Licence.pdf',
+    data_url='https://catalogue.data.gov.bc.ca/dataset/9530a41d-6484-41e5-b694-acb76e212a58/resource/34eedf53-c60b-4237-bf6e-81228a51ab12/download/edsre2015.zip',
+    encoding='iso-8859-1',
+    extra={'division_id': 'ocd-division/country:ca/province:bc'},
+)
