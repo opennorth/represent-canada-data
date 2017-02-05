2d6c657e7ee65bc66e1647dbae6dd125570a5426
James McKinney <james@slashpoundbang.com>
2017-02-04 21:48:01 -0500

    province:nb/2013: Update license

--- boundaries/ocd-division/country:ca/province:nb/2013/definition.py
+++ boundaries/ocd-division/country:ca/province:nb/2013/definition.py
@@ -12,8 +12,8 @@ boundaries.register('New Brunswick electoral districts',  # (2013)
     id_func=boundaries.attr('DIST_ID'),
     authority='Her Majesty the Queen in Right of New Brunswick',
     source_url='http://www.snb.ca/geonb1/e/DC/catalogue-E.asp',
+    licence_url='http://geonb.snb.ca/documents/license/geonb-odl_en.pdf',
     data_url='http://geonb.snb.ca/downloads/prov_electoral_districts/geonb_2014_ped-cep_shp.zip',
-    licence_url='http://geonb.snb.ca/downloads/documents/geonb_license_e.pdf',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/province:nb'},
 )
