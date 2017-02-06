dce15eadf21687a8bdd460236f4d3905daa8b08b
James McKinney <james@slashpoundbang.com>
2017-02-06 17:01:22 -0500

    Fix Saskatoon

--- boundaries/ocd-division/country:ca/csd:4711066/definition.py
+++ boundaries/ocd-division/country:ca/csd:4711066/definition.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+import re
 from datetime import date
 
 import boundaries
@@ -9,6 +10,7 @@ boundaries.register('Saskatoon wards',
     domain='Saskatoon, SK',
     last_updated=date(2016, 10, 14),
     name_func=lambda f: f.get('Name').replace(':', ''),
+    id_func=lambda f: re.sub(r'\D', '', f.get('Name')),
     authority='City of Saskatoon',
     source_url='http://opendata-saskatoon.cloudapp.net/DataBrowser/SaskatoonOpenDataCatalogueBeta/CurrentWardBoundaries',
     data_url='https://saskatoonopendatastorage.blob.core.windows.net/converteddata/kml.CurrentWardBoundaries.zip',
