85e59c29279866cbbab256c7cbef32c0e8c9f1f1
James McKinney <james@slashpoundbang.com>
2017-02-07 09:45:37 -0500

    Update Haldimand County

--- boundaries/ocd-division/country:ca/csd:3528018/definition.py
+++ boundaries/ocd-division/country:ca/csd:3528018/definition.py
@@ -7,13 +7,13 @@ import boundaries
 
 boundaries.register('Haldimand County wards',
     domain='Haldimand County, ON',
-    last_updated=date(2016, 10, 5),
+    last_updated=date(2017, 2, 7),
     name_func=boundaries.attr('Ward'),
     id_func=lambda f: re.sub(r'\D', '', f.get('Ward')),
     authority='Corporation of Haldimand County',
-    source_url='http://opendata.haldimandcounty.on.ca/datasets/8e0ac38a4c96452ab3c9900a8251e42c_49',
+    source_url='http://opendata.haldimandcounty.on.ca/datasets/wards',
     licence_url='http://opendata.haldimandcounty.on.ca/',
-    data_url='http://opendata.haldimandcounty.on.ca/datasets/8e0ac38a4c96452ab3c9900a8251e42c_49.zip',
+    data_url='https://opendata.arcgis.com/datasets/8e0ac38a4c96452ab3c9900a8251e42c_48.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3528018'},
 )
