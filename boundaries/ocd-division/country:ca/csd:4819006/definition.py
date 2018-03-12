572e244bc08f20c1420e8fd1030d504d11f52cad
James McKinney <james@slashpoundbang.com>
2018-03-11 23:43:12 -0400

    ocd-division/country:ca/csd:4711066 ocd-division/country:ca/csd:4819006: Update source

--- boundaries/ocd-division/country:ca/csd:4819006/definition.py
+++ boundaries/ocd-division/country:ca/csd:4819006/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Grande Prairie County No. 1 divisions',
     domain='Grande Prairie County No. 1, AB',
-    last_updated=date(2012, 6, 18),
+    last_updated=date(2017, 12, 1),
     name_func=lambda f: 'Division %s' % f.get('ElectoralD'),
     id_func=boundaries.attr('ElectoralD'),
     authority='County of Grande Prairie No. 1',
-    source_url='http://data.countygp.ab.ca/',
+    source_url='https://data1-cogp.opendata.arcgis.com/datasets/aec068b7976e432f99b7cbd89fe2abc7_0',
     licence_url='http://www.countygp.ab.ca/EN/main/community/maps-gis/open-data/open-data-licence.html',
-    data_url='http://data.countygp.ab.ca/data/CouncillorDivisions/Councillor_Divisions.zip',
+    data_url='https://opendata.arcgis.com/datasets/aec068b7976e432f99b7cbd89fe2abc7_0.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:4819006'},
 )
