5079506f41d3a0ac1a6d70b365bc1a6844b499b9
James McKinney <james@slashpoundbang.com>
2018-03-11 22:52:17 -0400

    ocd-division/country:ca/csd:3524002 ocd-division/country:ca/csd:3529006: Update source

--- boundaries/ocd-division/country:ca/csd:3524002/definition.py
+++ boundaries/ocd-division/country:ca/csd:3524002/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Burlington wards',
     domain='Burlington, ON',
-    last_updated=date(2016, 11, 22),
+    last_updated=date(2018, 3, 8),
     name_func=lambda f: 'Ward %s' % f.get('WARD_NO'),
     id_func=boundaries.attr('WARD_NO'),
     authority='City of Burlington',
-    source_url='http://cob.burlington.opendata.arcgis.com/datasets/3eba66ef43b0499d86d9e72aef66b65a_22',
-    licence_url='http://www.burlington.ca/en/services-for-you/resources/Ongoing_Projects/Open_Data/OpenDataBurlingtonTermsOfUseSeptember192011.pdf',
-    data_url='http://cob.burlington.opendata.arcgis.com/datasets/3eba66ef43b0499d86d9e72aef66b65a_22.zip',
+    source_url='https://navburl-burlington.opendata.arcgis.com/datasets/ward-boundaries',
+    licence_url='https://www.burlington.ca/en/services-for-you/resources/Ongoing_Projects/Open_Data/OpenDataBurlingtonTermsOfUseSeptember192011.pdf',
+    data_url='https://opendata.arcgis.com/datasets/73bee9ff196141a2a1e45be8c04ae404_0.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3524002'},
 )
--- boundaries/ocd-division/country:ca/csd:3529006/definition.py
+++ boundaries/ocd-division/country:ca/csd:3529006/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Brantford wards',
     domain='Brantford, ON',
-    last_updated=date(2016, 8, 12),
+    last_updated=date(2017, 5, 9),
     name_func=boundaries.attr('WARD_NAME'),
     id_func=boundaries.attr('WARD'),
     authority='City of Brantford',
-    source_url='http://data.brantford.opendata.arcgis.com/datasets/10acbe7b336a428e892b8aa5f3383fbe_0',
-    licence_url='http://data.brantford.opendata.arcgis.com/',
-    data_url='http://data.brantford.opendata.arcgis.com/datasets/10acbe7b336a428e892b8aa5f3383fbe_0.zip',
+    source_url='http://data-brantford.opendata.arcgis.com/datasets/city-of-brantford-ward-boundaries',
+    licence_url='http://data-brantford.opendata.arcgis.com/pages/licence',
+    data_url='https://opendata.arcgis.com/datasets/6bc1504473ba4235b58494af5bf05227_0.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3529006'},
 )
