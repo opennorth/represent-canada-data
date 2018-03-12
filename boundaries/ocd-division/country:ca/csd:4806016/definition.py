ad3221b6830eaed85e48e6ea807024b4dd70f2f2
James McKinney <james@slashpoundbang.com>
2018-03-11 23:21:29 -0400

    ocd-division/country:ca/csd:4806016 ocd-division/country:ca/csd:4811061: Update source

--- boundaries/ocd-division/country:ca/csd:4806016/definition.py
+++ boundaries/ocd-division/country:ca/csd:4806016/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Calgary wards',
     domain='Calgary, AB',
-    last_updated=date(2017, 10, 6),
-    name_func=boundaries.clean_attr('LABEL'),
-    id_func=lambda f: int(f.get('WARD_NUM')),
+    last_updated=date(2018, 3, 11),
+    name_func=boundaries.clean_attr('label'),
+    id_func=lambda f: int(f.get('ward_num')),
     authority='City of Calgary',
-    source_url='https://data.calgary.ca/Government/Ward-Boundaries-effective-October-16-2017-/4f6r-atgm',
+    source_url='https://data.calgary.ca/Government/Ward-Boundaries/r9vx-mhnf',
     licence_url='https://data.calgary.ca/stories/s/u45n-7awa',
-    data_url='https://data.calgary.ca/api/geospatial/4f6r-atgm?method=export&format=Shapefile',
+    data_url='https://data.calgary.ca/api/geospatial/r9vx-mhnf?method=export&format=Shapefile',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:4806016'},
 )
--- boundaries/ocd-division/country:ca/csd:4811061/definition.py
+++ boundaries/ocd-division/country:ca/csd:4811061/definition.py
@@ -1,17 +1,16 @@
-import re
 from datetime import date
 
 import boundaries
 
 boundaries.register('Edmonton wards',
     domain='Edmonton, AB',
-    last_updated=date(2017, 2, 6),
-    name_func=lambda f: 'Ward %s' % re.sub(r'\D+0?', '', f.get('name')),
-    id_func=lambda f: re.sub(r'\D+0?', '', f.get('name')),
+    last_updated=date(2018, 3, 11),
+    name_func=lambda f: 'Ward %s' % f.get('name'),
+    id_func=boundaries.attr('name'),
     authority='City of Edmonton',
-    source_url='https://data.edmonton.ca/Administrative/City-of-Edmonton-Ward-Boundaries/yhng-294h?',
+    source_url='https://data.edmonton.ca/Administrative/City-of-Edmonton-Ward-Boundaries-effective-at-12-0/xre8-g6yf',
     licence_url='https://www.edmonton.ca/city_government/documents/Web-version2.1-OpenDataAgreement.pdf',
-    data_url='https://data.edmonton.ca/api/geospatial/yhng-294h?method=export&format=Shapefile',
+    data_url='https://data.edmonton.ca/api/geospatial/xre8-g6yf?method=export&format=Shapefile',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:4811061'},
 )
