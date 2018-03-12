49f64143773553bd7f057b0983c30b59b269b772
James McKinney <james@slashpoundbang.com>
2018-03-11 22:43:40 -0400

    ocd-division/country:ca/csd:1209034: Update source

--- boundaries/ocd-division/country:ca/csd:1209034/definition.py
+++ boundaries/ocd-division/country:ca/csd:1209034/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Halifax districts',
     domain='Halifax, NS',
-    last_updated=date(2016, 11, 5),
+    last_updated=date(2017, 10, 31),
     name_func=boundaries.clean_attr('DISTNAME'),
     id_func=boundaries.attr('DIST_ID'),
     authority='Halifax Regional Municipality',
-    source_url='http://catalogue.hrm.opendata.arcgis.com/datasets/91dd3d978e3d4324bdb88f9fc5e675e4_0',
-    licence_url='http://www.halifax.ca/opendata/OD_TermsOfUse.php',
-    data_url='http://catalogue.hrm.opendata.arcgis.com/datasets/91dd3d978e3d4324bdb88f9fc5e675e4_0.zip',
+    source_url='http://catalogue-hrm.opendata.arcgis.com/datasets/polling-districts',
+    licence_url='https://www.halifax.ca/home/open-data/open-data-license',
+    data_url='https://opendata.arcgis.com/datasets/04c5bee564f84b4a8f1c8dd305896079_0.zip',
     encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/csd:1209034'},
 )
