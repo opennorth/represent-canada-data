41070a06ad514ce8d0dfee033ca2d968fafcfd88
James McKinney <james@slashpoundbang.com>
2018-08-13 18:14:15 -0400

    Update QC concordances

--- concordances/ocd-division/country:ca/province:qc/update.sh
+++ concordances/ocd-division/country:ca/province:qc/update.sh
@@ -1,7 +1,9 @@
 curl -s -O https://www.electionsquebec.qc.ca/documents/zip/CP_CIRC_MUN_MRC_RA_BRUT.zip
 unzip -o -q CP_CIRC_MUN_MRC_RA_BRUT.zip
 rm -f CP_CIRC_MUN_MRC_RA_BRUT.zip
-TMP=$LANG LC_CTYPE=en_CA.ISO8859-1
+export TMP=$LANG
+export LC_CTYPE=en_CA.ISO8859-1
 cut -d ";" -f 1-2 CP_CIRC_MUN_MRC_RA_BRUT.txt | tail +2 | uniq | sed 's/;/,/' > out.csv
-LC_CTYPE=$TMP
+export LC_CTYPE=$TMP
+unset TMP
 rm -f CP_CIRC_MUN_MRC_RA_BRUT.txt
