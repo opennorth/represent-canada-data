8731fa87ee676ebdfea886e68629d3b0128c5756
James McKinney <james@slashpoundbang.com>
2018-09-11 19:50:38 -0400

    qc: Fix bug in concordance extraction

--- concordances/ocd-division/country:ca/province:qc/update.sh
+++ concordances/ocd-division/country:ca/province:qc/update.sh
@@ -3,7 +3,7 @@ unzip -o -q CP_CIRC_MUN_MRC_RA_BRUT.zip
 rm -f CP_CIRC_MUN_MRC_RA_BRUT.zip
 export TMP=$LANG
 export LC_CTYPE=en_CA.ISO8859-1
-cut -d ";" -f 1-2 CP_CIRC_MUN_MRC_RA_BRUT.txt | tail +2 | uniq | sed 's/;/,/' > out.csv
+cut -d ";" -f 1,11 CP_CIRC_MUN_MRC_RA_BRUT.txt | tail +2 | uniq | sed 's/;/,/' > out.csv
 export LC_CTYPE=$TMP
 unset TMP
 rm -f CP_CIRC_MUN_MRC_RA_BRUT.txt
