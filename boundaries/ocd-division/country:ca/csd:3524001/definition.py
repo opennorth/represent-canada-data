830c1a7762cf3e50f284ccaf6378ed4eee460301
James McKinney <james@slashpoundbang.com>
2018-11-05 19:30:19 -0500

    Update Oakville wards

--- boundaries/ocd-division/country:ca/csd:3524001/definition.py
+++ boundaries/ocd-division/country:ca/csd:3524001/definition.py
@@ -4,14 +4,13 @@ import boundaries
 
 boundaries.register('Oakville wards',
     domain='Oakville, ON',
-    last_updated=date(2016, 4, 1),
-    name_func=boundaries.attr('FULL_NAME'),
-    id_func=boundaries.attr('WARD'),
+    last_updated=date(2018, 6, 11),
+    name_func=lambda f: 'Ward %s' % f.get('Ward'),
+    id_func=boundaries.attr('Ward'),
     authority='Town of Oakville',
-    # 2018-10-22: https://portal-exploreoakville.opendata.arcgis.com/datasets/toak::2018-ward-boundaries
-    source_url='https://portal-exploreoakville.opendata.arcgis.com/datasets/toak::ward-boundaries',
+    source_url='https://portal-exploreoakville.opendata.arcgis.com/datasets/toak::2018-ward-boundaries',
     licence_url='https://www.oakville.ca/data/open_data_licence.html',
-    data_url='https://opendata.arcgis.com/datasets/06e1062918f043258ebcada195527120_0.zip',
+    data_url='https://opendata.arcgis.com/datasets/36137fb86deb4431b53450cf16370a3b_0.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3524001'},
 )
