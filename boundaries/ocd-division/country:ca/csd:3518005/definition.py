21a208b11eb716636d781e5f594e6ca182d58668
James McKinney <james@slashpoundbang.com>
2018-11-05 19:53:01 -0500

    Update Ajax wards

--- boundaries/ocd-division/country:ca/csd:3518005/definition.py
+++ boundaries/ocd-division/country:ca/csd:3518005/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Ajax wards',
     domain='Ajax, ON',
-    last_updated=date(2017, 6, 13),
-    name_func=lambda f: 'Ward %s' % f.get('WARD'),
-    id_func=boundaries.attr('WARD'),
+    last_updated=date(2018, 11, 4),
+    name_func=lambda f: 'Ward %s' % f.get('WARD_NUM'),
+    id_func=boundaries.attr('WARD_NUM'),
     authority='Town of Ajax',
-    source_url='http://opendata.ajax.ca/datasets/f441095b72ad4c3da2638a73a0c9e412_23',
+    source_url='http://opendata.ajax.ca/datasets/2018-ward-boundaries',
     licence_url='http://townofajax.maps.arcgis.com/sharing/rest/content/items/22e2d8e248724d7cb0310dc2db675abd/data',
-    data_url='http://opendata.ajax.ca/datasets/f441095b72ad4c3da2638a73a0c9e412_23.zip',
+    data_url='https://opendata.arcgis.com/datasets/c45b387629404a6fb0a2f59f501b124c_28.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3518005'},
 )
