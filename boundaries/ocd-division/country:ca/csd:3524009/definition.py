3a6397238f289097b0d09ea80acb3309fd2ea580
James McKinney <james@slashpoundbang.com>
2018-11-05 19:29:13 -0500

    Update Milton wards

--- boundaries/ocd-division/country:ca/csd:3524009/definition.py
+++ boundaries/ocd-division/country:ca/csd:3524009/definition.py
@@ -5,14 +5,13 @@ import boundaries
 
 boundaries.register('Milton wards',
     domain='Milton, ON',
-    last_updated=date(2017, 2, 6),
-    name_func=lambda f: 'Ward %s' % re.search(r'WARD (\d+)', f.get('Descriptio')).group(1),
-    id_func=lambda f: re.search(r'WARD (\d+)', f.get('Descriptio')).group(1),
+    last_updated=date(2018, 7, 16),
+    name_func=boundaries.clean_attr('WARD'),
+    id_func=boundaries.attr('WARD_NUM'),
     authority='Town of Milton',
     source_url='https://www.milton.ca/en/townhall/opendata.asp',
     licence_url='https://www.milton.ca/en/townhall/resources/OpenDataAgreement.pdf',
-    # 2018-10-22: https://www.milton.ca/en/resources/FutureWardBoundaries_180620128.zip
-    data_url='https://www.milton.ca/en/resources/WardBoundaries_18062018.zip',
+    data_url='http://www.milton.ca/en/resources/FutureWardBoundaries_180620128.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3524009'},
 )
