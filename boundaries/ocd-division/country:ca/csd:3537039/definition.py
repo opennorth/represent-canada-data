a4d0447571941c73cdaae88ebf9c03826c77a062
James McKinney <james@slashpoundbang.com>
2018-11-05 19:24:52 -0500

    Update Windsor wards

--- boundaries/ocd-division/country:ca/csd:3537039/definition.py
+++ boundaries/ocd-division/country:ca/csd:3537039/definition.py
@@ -5,13 +5,13 @@ import boundaries
 
 boundaries.register('Windsor wards',
     domain='Windsor, ON',
-    last_updated=date(2014, 7, 31),
+    last_updated=date(2018, 9, 21),
     name_func=boundaries.clean_attr('WARD'),
-    id_func=lambda f: re.sub(r'\D', '', f.get('WARD')),
+    id_func=boundaries.attr('NUMBER'),
     authority='City of Windsor',
     source_url='https://opendata.citywindsor.ca/opendata/details/222',
     licence_url='https://citywindsor.ca/opendata/Documents/OpenDataTermsofUse.pdf',
-    data_url='https://opendata.citywindsor.ca/Uploads/2014_Municipal_Ward_Boundaries_UTM83.zip',
+    data_url='https://opendata.citywindsor.ca/Uploads/2018_Municipal_Ward_Boundaries_UTM83.zip',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3537039'},
 )
