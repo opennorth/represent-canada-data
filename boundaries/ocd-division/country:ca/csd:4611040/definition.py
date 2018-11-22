a0f730078ecbc523a96f4237e62469dbc81a9a1f
James McKinney <james@slashpoundbang.com>
2018-11-22 16:05:45 -0500

    Update Winnipeg wards

--- boundaries/ocd-division/country:ca/csd:4611040/definition.py
+++ boundaries/ocd-division/country:ca/csd:4611040/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Winnipeg wards',
     domain='Winnipeg, MB',
-    last_updated=date(2018, 8, 4),
+    last_updated=date(2018, 11, 22),
     name_func=boundaries.clean_attr('name'),
-    id_func=lambda f: int(f.get('number')),
+    id_func=lambda f: int(f.get('id')),
     authority='City of Winnipeg',
-    source_url='https://data.winnipeg.ca/Council-Services/Electoral-Ward/ede3-teb8',
+    source_url='https://data.winnipeg.ca/Council-Services/Electoral-Boundaries-as-of-September-17th-2018/qake-hdwc',
     licence_url='https://data.winnipeg.ca/open-data-licence',
-    data_url='https://data.winnipeg.ca/api/geospatial/ede3-teb8?method=export&format=Shapefile',
+    data_url='https://data.winnipeg.ca/api/geospatial/qake-hdwc?method=export&format=Shapefile',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:4611040'},
 )
