f3bebd97693fcd533e3efa53b15dc93096fac56a
James McKinney <james@slashpoundbang.com>
2018-11-26 14:05:08 -0500

    Update Peterborough wards

--- boundaries/ocd-division/country:ca/csd:3515014/definition.py
+++ boundaries/ocd-division/country:ca/csd:3515014/definition.py
@@ -6,15 +6,16 @@ import boundaries
 
 def namer(f):
     import boundaries
-    return re.sub(r' Ward \d+\Z', '', boundaries.clean_attr('NAME')(f))
+    return re.sub(r' Ward \d+\Z', '', boundaries.clean_attr('WARDNAME')(f))
 
 
 boundaries.register('Peterborough wards',
     domain='Peterborough, ON',
-    last_updated=date(2014, 2, 28),
+    last_updated=date(2018, 12, 3),
     name_func=namer,
-    id_func=lambda f: re.sub(r'\D', '', f.get('NAME')),
+    id_func=lambda f: re.sub(r'\D', '', f.get('WARDNAME')),
     authority='City of Peterborough',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/csd:3515014'},
+    is_valid_func=lambda f: int(f.get('YR')) == '2017' and f.get('WARDTYPE') == 'WARD',
 )
