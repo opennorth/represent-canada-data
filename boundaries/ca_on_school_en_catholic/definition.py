f8f28fed9ac775c4c403e0d77c6dd4d6ee5f1df1
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-07-21 16:18:16 -0400

    Add English Catholic School Board boundaries

--- boundaries/ca_on_school_en_catholic/definition.py
+++ boundaries/ca_on_school_en_catholic/definition.py
@@ -2,15 +2,14 @@ from datetime import date
 
 import boundaries
 
-boundaries.register('Ontario English Catholic School Board',
-    domain='Canada',
+boundaries.register('Ontario English Catholic School Board boundaries',
+    domain='Ontario',
     last_updated=date(2015, 11, 8),
-    name_func=boundaries.attr('CDNAME'),
-    id_func=boundaries.attr('CDUID'),
-    slug_func=boundaries.attr('CDUID'),
-    authority='Land Information Ontario',
+    name_func=boundaries.attr('NAME'),
+    id_func=boundaries.attr('SDSB_ID'),
+    authority='Her Majesty the Queen in Right of Ontario',
     source_url='https://geohub.lio.gov.on.ca/datasets/school-board-boundaries',
-    licence_url='https://geohub.lio.gov.on.ca/datasets/school-board-boundaries#',
+    licence_url='https://geohub.lio.gov.on.ca/datasets/school-board-boundaries',
     data_url='https://ws.gisetl.lrc.gov.on.ca/fmedatadownload/Packages/SchoolBoardBoundaries.zip',
     encoding='utf-8',
 )
