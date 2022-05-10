21436d28314bcfb27eee8a078f6dcece45aa1b10
Simon Meers <simon@simonmeers.com>
2022-05-10 21:34:56 +1000

    add 2018 NB districts.

--- boundaries/ocd-division/country:ca/province:nb/2018/definition.py
+++ boundaries/ocd-division/country:ca/province:nb/2018/definition.py
@@ -2,16 +2,15 @@ from datetime import date
 
 import boundaries
 
-boundaries.register('New Brunswick electoral districts (2013)',
+boundaries.register('New Brunswick electoral districts (2018)',
     singular='New Brunswick electoral district',
     domain='New Brunswick',
-    last_updated=date(2015, 6, 23),
+    last_updated=date(2022, 5, 10),
     name_func=boundaries.attr('PED_Name_E'),
     id_func=boundaries.attr('DIST_ID'),
     authority='Her Majesty the Queen in Right of New Brunswick',
-    source_url='http://www.snb.ca/geonb1/e/DC/catalogue-E.asp',
-    licence_url='http://geonb.snb.ca/documents/license/geonb-odl_en.pdf',
-    data_url='http://geonb.snb.ca/downloads/prov_electoral_districts/geonb_2014_ped-cep_shp.zip',
+    source_url='https://www.electionsnb.ca/content/enb/en/maps/PED.html',
+    data_url='https://www.electionsnb.ca/content/dam/enb/pdf/2016PEDMaps-CEPCartes/Provincial_Electoral_Districts_2018.zip',
     encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/province:nb'},
 )
