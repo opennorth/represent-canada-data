089cacd09b4186f8d61bafd86d7c2af67fe8721c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-12-09 18:34:40 -0500

    Update Newmarket

--- boundaries/ocd-division/country:ca/csd:3519048/definition.py
+++ boundaries/ocd-division/country:ca/csd:3519048/definition.py
@@ -4,12 +4,13 @@ import boundaries
 
 boundaries.register('Newmarket wards',
     domain='Newmarket, ON',
-    last_updated=date(2016, 8, 6),
+    last_updated=date(2023, 10, 4),
     name_func=lambda f: 'Ward %s' % f.get('WARD'),
     id_func=boundaries.attr('WARD'),
     authority='Town of Newmarket',
-    encoding='iso-8859-1',
-    source_url='http://open.newmarket.ca/opendata/navigo/#/show/4e404ea7c3cb6e3c?disp=6b625f5',
+    source_url='https://navigate-newmarket.hub.arcgis.com/datasets/f0537eeb8e7e4f8f8f9922083e34d0e0_0/about',
+    data_url='https://opendata.arcgis.com/api/v3/datasets/f0537eeb8e7e4f8f8f9922083e34d0e0_0/downloads/data?format=shp&spatialRefId=4326&where=1%3D1',
     licence_url='https://www.newmarket.ca/TownGovernment/Documents/Newmarket_OpenData_Licence.pdf',
+    encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/csd:3519048'},
 )
