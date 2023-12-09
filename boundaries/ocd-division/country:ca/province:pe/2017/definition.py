dc0a174c8a3378d7bedba5740d35fef37bb9ea5f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-12-09 18:15:54 -0500

    licensing: Add or update licenses for NB, NS

--- boundaries/ocd-division/country:ca/province:pe/2017/definition.py
+++ boundaries/ocd-division/country:ca/province:pe/2017/definition.py
@@ -11,10 +11,7 @@ boundaries.register('Prince Edward Island electoral districts (2017)',
     id_func=boundaries.attr('DIST_NO'),
     authority='Her Majesty the Queen in Right of Prince Edward Island',
     source_url='http://www.gov.pe.ca/gis/index.php3?number=77554&lang=E',
-    data_url=(
-        'http://www.gov.pe.ca/gis/download.php3?'
-        'name=2017_Electoral&file_format=SHP'
-    ),
+    data_url='http://www.gov.pe.ca/gis/download.php3?name=2017_Electoral&file_format=SHP',
     licence_url='http://www.gov.pe.ca/gis/index.php3?number=77462&lang=E',
     encoding='iso-8859-1',
     extra={'division_id': 'ocd-division/country:ca/province:pe'},
