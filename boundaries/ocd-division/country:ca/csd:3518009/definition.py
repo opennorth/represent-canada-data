c09d632111ada2dfa146c0731679760f19844c1b
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-12-09 18:33:26 -0500

    Update Whitby

--- boundaries/ocd-division/country:ca/csd:3518009/definition.py
+++ boundaries/ocd-division/country:ca/csd:3518009/definition.py
@@ -4,10 +4,13 @@ import boundaries
 
 boundaries.register('Whitby wards',
     domain='Whitby, ON',
-    last_updated=date(2013, 1, 8),
+    last_updated=date(2023, 2, 15),
     name_func=boundaries.attr('WARD_DESC'),
-    id_func=lambda f: f.get('WARD_TEXT').replace('0', ''),
+    id_func=boundaries.attr('WARD'),
+    source_url='https://geohub-whitby.hub.arcgis.com/datasets/Whitby::ward-boundaries/about',
+    data_url='https://opendata.arcgis.com/api/v3/datasets/31ab4780b412445db594aaadc2bf0de2_0/downloads/data?format=shp&spatialRefId=26917&where=1%3D1',
+    licence_url='https://whitby.maps.arcgis.com/sharing/rest/content/items/223810efc31c40b3aff99dd74f809a97/data',
     authority='Town of Whitby',
-    encoding='iso-8859-1',
+    encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/csd:3518009'},
 )
