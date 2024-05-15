3c5ee79abe4a924d747730a2ff94c733cd1afaa8
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-15 18:43:48 -0400

    fix(nt/2013): Match names to legislature

--- boundaries/ocd-division/country:ca/territory:nt/2013/definition.py
+++ boundaries/ocd-division/country:ca/territory:nt/2013/definition.py
@@ -2,12 +2,26 @@ from datetime import date
 
 import boundaries
 
+
+def namer(f):
+    import boundaries
+    n = boundaries.attr('ED')(f)
+    # @see bhttps://www.ntlegislativeassembly.ca/members/members-legislative-assembly/members
+    if n == 'Deh Cho':
+        return 'Dehcho'
+    if n == 'Tu Nedhe':
+        return 'Tu Nedhé-Wiilideh'
+    if n == 'Weledeh':
+        return "Yellowknife North"
+    return n
+
+
 boundaries.register('Northwest Territories electoral districts (2013)',
     singular='Northwest Territories electoral district',
     domain='Northwest Territories',
     # https://nwtelectoralboundaries.ca/wp-content/uploads/sites/6/2022/05/2021-electoral-boundaries-final-report-web.pdf
     last_updated=date(2022, 5, 24),
-    name_func=boundaries.attr('ED'),
+    name_func=namer,
     id_func=lambda f: int(f.get('EDNWTF_ID')),
     authority='Her Majesty the Queen in Right of Northwest Territories',
     encoding='iso-8859-1',
