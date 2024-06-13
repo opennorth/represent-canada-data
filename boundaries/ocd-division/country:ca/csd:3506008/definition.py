88afe33556984a6e0d65d71f0436c5a273778a92
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-06-13 16:23:33 -0400

    Update Ottawa

--- boundaries/ocd-division/country:ca/csd:3506008/definition.py
+++ boundaries/ocd-division/country:ca/csd:3506008/definition.py
@@ -4,13 +4,13 @@ import boundaries
 
 boundaries.register('Ottawa wards',
     domain='Ottawa, ON',
-    last_updated=date(2018, 4, 24),
+    last_updated=date(2024, 6, 13),
     name_func=boundaries.clean_attr('WARD_EN'),
     id_func=boundaries.attr('WARD_NUM'),
     authority='City of Ottawa',
-    source_url='http://data.ottawa.ca/en/dataset/wards-2014',
+    source_url='https://open.ottawa.ca/datasets/ottawa::wards-2022-2026/about',
     licence_url='https://ottawa.ca/en/city-hall/get-know-your-city/open-data#open-data-licence-version-2-0',
-    data_url='http://data.ottawa.ca/dataset/8321248d-0b86-47cd-9c83-c848a1bc0098/resource/8152e707-fc28-409c-9bbe-8cb9f88dca86/download/wards-2014shp.shp.zip',
+    data_url='https://opendata.arcgis.com/api/v3/datasets/5aec8bb2e9c84132afe9522ce513afb8_105/downloads/data?format=shp&spatialRefId=4326&where=1%3D1',
     encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/csd:3506008'},
 )
