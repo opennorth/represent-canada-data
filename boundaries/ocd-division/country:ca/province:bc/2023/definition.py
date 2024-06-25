bc611dbdfa68e5ef39689d5b3c7c0cbf61522640
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-06-25 11:00:34 -0400

    Use final British Columbia (2023) boundaries

--- boundaries/ocd-division/country:ca/province:bc/2023/definition.py
+++ boundaries/ocd-division/country:ca/province:bc/2023/definition.py
@@ -6,12 +6,12 @@ boundaries.register('British Columbia electoral districts (2023 Redistribution)'
     singular='British Columbia electoral district',
     domain='British Columbia',
     last_updated=date(2023, 3, 29),
-    name_func=boundaries.attr('Dist_Name'),
-    id_func=boundaries.attr('Dist_ID'),
+    name_func=boundaries.attr('ED_NAME'),
+    id_func=boundaries.attr('ED_ABBREV'),
     authority='Her Majesty the Queen in Right of British Columbia',
     source_url='https://catalogue.data.gov.bc.ca/dataset/provincial-electoral-districts-electoral-boundaries-redistribution-2023',
     licence_url='https://www.elections.bc.ca/docs/EBC-Open-Data-Licence.pdf',
-    data_url='https://bcebc.ca/wp-content/uploads/Proposed_Electoral_Districts._shp.zip',
+    # Google Chrome: "Export data or view details from the BC Geographic Warehouse (custom download)" "Access/Download"
     encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/province:bc'},
 )
