6acdbaf0dc7e4a9db03b71d1ed9bbd573e51a748
Seamus Lee <seamuslee001@gmail.com>
2025-03-06 10:05:28 +1100

    Fix data url

--- boundaries/ocd-division/country:ca/2023/definition.py
+++ boundaries/ocd-division/country:ca/2023/definition.py
@@ -18,7 +18,7 @@ boundaries.register('2023 Federal electoral districts',  # (2023 Representation 
     authority='Her Majesty the Queen in Right of Canada',
     source_url='https://open.canada.ca/data/en/dataset/18bf3ea7-1940-46ec-af52-9ba3f77ed708',
     licence_url='https://open.canada.ca/en/open-government-licence-canada',
-    data_url='https://www12.statcan.gc.ca/census-recensement/2011/geo/bound-limit/files-fichiers/2016/lfed000a16a_e.zip',
+    data_url='https://ftp.maps.canada.ca/pub/elections_elections/Electoral-districts_Circonscription-electorale/federal_electoral_districts_boundaries_2023/FED_CA_2023_EN-SHP.zip',
     encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca'},
 )
