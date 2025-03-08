d2e097591a89af0cc84a54bce1c3a009f3a47c4f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-03-08 15:42:31 +0100

    fix: Correct ca/2023 definition.py

--- boundaries/ocd-division/country:ca/2023/definition.py
+++ boundaries/ocd-division/country:ca/2023/definition.py
@@ -8,13 +8,13 @@ def namer(f):
     return boundaries.clean_attr('ED_NAMEE')(f).replace('\u0092', "'")
 
 
-boundaries.register('2023 Federal electoral districts',  # (2023 Representation Order)
-    singular='2023 Federal electoral district',
+boundaries.register('Federal electoral districts (2023 Representation Order)',
+    singular='Federal electoral district',
     domain='Canada',
-    last_updated=date(2023, 2, 25),
+    last_updated=date(2024, 12, 17),
     name_func=namer,
-    id_func=boundaries.attr('FED_NUM '),
-    slug_func=boundaries.attr('FED_NUM '),
+    id_func=boundaries.attr('FED_NUM'),
+    slug_func=boundaries.attr('FED_NUM'),
     authority='Her Majesty the Queen in Right of Canada',
     source_url='https://open.canada.ca/data/en/dataset/18bf3ea7-1940-46ec-af52-9ba3f77ed708',
     licence_url='https://open.canada.ca/en/open-government-licence-canada',
