751f9c4035e819d2e44ad5fe29d1f99ccab1063c
James McKinney <26463+jpmckinney@users.noreply.github.com>
2025-03-18 16:55:37 -0400

    Remove Halifax data_url (need to click to download to generate new file)

--- boundaries/ocd-division/country:ca/csd:1209034/definition.py
+++ boundaries/ocd-division/country:ca/csd:1209034/definition.py
@@ -10,7 +10,6 @@ boundaries.register('Halifax districts',
     authority='Halifax Regional Municipality',
     source_url='https://data-hrm.hub.arcgis.com/datasets/04c5bee564f84b4a8f1c8dd305896079_0',
     licence_url='https://data-hrm.hub.arcgis.com/pages/open-data-licence',
-    data_url='https://data-hrm.hub.arcgis.com/datasets/04c5bee564f84b4a8f1c8dd305896079_0.zip',
     encoding='utf-8',
     extra={'division_id': 'ocd-division/country:ca/csd:1209034'},
 )
